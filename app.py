e107eaf6d25c6fb69ad5840f84b18dcd223f6060
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-08-10 21:00:42 -0700

    update display

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-# config                    
-from flask import Flask
-app = Flask(__name__)
-
-# Index route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFax!'
-
-# Pets Index Route
-@app.route('/pets')
-def pets_index():
-    return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
